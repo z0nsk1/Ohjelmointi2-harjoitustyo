c52c9710cc3c6bca1e2957a51c0b68f9aab53f8e
jonnurmi <nurmjo@gmail.com>
2020-03-24 18:39:40 +0200

    Jatkoa jasen ja harjoitusluokille

--- src/fxJasenrekisteri/Harjoitukset.java
+++ src/fxJasenrekisteri/Harjoitukset.java
@@ -1,10 +1,82 @@
 package fxJasenrekisteri;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+
 /**
  * @author z0nsk1
  * @version 19.3.2020
  *
  */
 public class Harjoitukset {
-//
+    
+    private String tiedostonNimi = "";
+    
+    /** 
+     * Taulukko harjoituksista (kokoelma)
+     */
+    private final Collection<Harjoitus> harjoitukset = new ArrayList<Harjoitus>();
+    
+    
+    /**
+     * Alustaja
+     */
+    public Harjoitukset() {
+        // 
+    }
+    
+    
+    /**
+     * @param hakemisto jossa tiedosto sijaitsee
+     * @throws SailoException jos ei onnistu
+     */
+    public void lueTiedostosta(String hakemisto) throws SailoException {
+        tiedostonNimi = hakemisto + ".harj";
+        throw new SailoException("Ei osata vielä lukea tiedostoa " + tiedostonNimi);
+    }
+    
+    
+    /**
+     * @return harjoitukset-taulukon koko
+     */
+    public int getLkm() {
+        return harjoitukset.size();
+    }
+    
+    
+    /**
+     * 
+     * @param harj lisattava harjoitus
+     */
+    public void lisaa(Harjoitus harj) {
+        harjoitukset.add(harj);
+    }
+    
+    
+    /**
+     * Aliohjelma tallentamiselle
+     * @throws SailoException jos ei onnistu
+     */
+    public void tallenna() throws SailoException {
+        throw new SailoException("Ei osata vielä tallentaa tiedostoa " + tiedostonNimi);
+    }
+    
+    
+    /**
+     * @param args ei kaytossa
+     */
+    public static void main(String[] args) {
+        Harjoitukset harjoitukset1 = new Harjoitukset();
+        Harjoitus testi1 = new Harjoitus();
+        testi1.pvm();
+        testi1.hTiedot();
+        Harjoitus testi2 = new Harjoitus();
+        testi2.pvm();
+        testi2.hTiedot();
+        
+        harjoitukset1.lisaa(testi1);
+        harjoitukset1.lisaa(testi2);
+
+    }
 }
--- src/fxJasenrekisteri/Harjoitus.java
+++ src/fxJasenrekisteri/Harjoitus.java
@@ -1,10 +1,69 @@
 package fxJasenrekisteri;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author z0nsk1
  * @version 19.3.2020
  *
  */
 public class Harjoitus {
-//
+    private String  harjoitusId = "202003201700";;
+    private int     paikalla      = 0;   //paikalla olleen jasenen id
+    private int     poissa;              
+    private String  hLisatietoja  = "";
+    
+    
+    /**
+     * Asetetaan harjoituksen tiedot
+     */
+    public void hTiedot() {
+        paikalla = 1; //jatkossa kutsuu kyseista jasenta jolla on tama id
+        poissa = -1; //poissa olleen jasenen id, -1 jos kukaan ei poissa/paikalla
+        hLisatietoja = "Hyvin porukkaa paikalla";
+    }
+    
+    
+    /**
+     * @param out tietovirta johon tulostetaan
+     */
+    public void tulosta(PrintStream out) {
+        out.println(harjoitusId);
+        out.println(paikalla); 
+        out.println(poissa);
+        out.println(hLisatietoja);
+    }
+    
+    
+    /**
+     * Tulostetaan harjoituksen tiedot
+     * @param os tietovirta johon tulostetaan
+     */
+    public void tulosta(OutputStream os) {
+        tulosta(new PrintStream(os));
+    }
+    
+    
+    /**
+     * Otetaan id tamanhetkisesta ajasta (oletus kun luodaan harjoitus)
+     */
+    public void pvm() {
+        DateTimeFormatter muoto = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+        LocalDateTime aika = LocalDateTime.now();
+        harjoitusId = muoto.format(aika);
+    }
+    
+    
+    /**
+     * @param args ei käytössä
+     */
+    public static void main(String[] args) {
+        Harjoitus harj = new Harjoitus();
+        harj.pvm();
+        harj.hTiedot();
+        harj.tulosta(System.out);
+    }
 }
